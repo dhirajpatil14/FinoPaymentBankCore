ae6262b0882464cfc13592d91b5ec7889363807d
viralc <viral_chauhan@finobank.com>
2022-07-12 19:37:19 +0530

    create test case method id 2

--- LoginServiceCore/Test/LoginService.API.Integration.Tests/Controllers/AccountControllerTests.cs
+++ LoginServiceCore/Test/LoginService.API.Integration.Tests/Controllers/AccountControllerTests.cs
@@ -15,12 +15,14 @@ namespace LoginService.API.Integration.Tests.Controllers
 
         private readonly CustomWebLoginApplicationFactory _factory;
 
-        private readonly string apiUrl = "https://localhost:44370/api/Account/";
+        private readonly string apiUrl = "https://localhost:44372/api/Account/";
 
         public AccountControllerTests(CustomWebLoginApplicationFactory factory)
         {
             _factory = factory;
         }
+
+        #region Method 1
 
         [Fact]
         public async Task Validate_User_ReturnsSuccessResult()
@@ -33,7 +35,7 @@ namespace LoginService.API.Integration.Tests.Controllers
                 TellerId = "100032353",
                 MethodId = 1,
                 IsEncrypt = false,
-                RequestData = "{\"user_id\":\"Gipxpl7iWL62b/ldd5suEA==\",\"client_id\":\"FINOMER\",\"ECBBlockEncryption\":true}"
+                RequestData = "{\"user_id\":\"K7CFFkt/XuWxCJZLlXpFQg==\",\"client_id\":\"FINOMER\",\"ECBBlockEncryption\":true}"
             };
 
             var eventJson = @userRequest.ToJsonSerialize();
@@ -41,32 +43,12 @@ namespace LoginService.API.Integration.Tests.Controllers
             var response = await client.PostAsync($"{apiUrl}Authenticate", content);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldNotBeNullOrEmpty();
             var result = responseString.ToJsonDeSerialize<OutResponse>();
+            result.ResponseData.ShouldNotBeNullOrEmpty();
             result.ResponseCode.ShouldBeEquivalentTo(0);
         }
 
-        [Fact]
-        public async Task CheckValidReturnCode()
-        {
-            var client = _factory.CreateDefaultClient();
-
-            var @userRequest = new AuthenticationRequest()
-            {
-                RequestId = "100032353_6292022153038769",
-                TellerId = "100032353",
-                MethodId = 1,
-                IsEncrypt = false,
-                RequestData = "{\"user_id\":\"K7CFFkt/==1\",\"client_id\":\"FINOMER\",\"ECBBlockEncryption\":true}"
-            };
-
-            var eventJson = @userRequest.ToJsonSerialize();
-            HttpContent content = new StringContent(eventJson, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync($"{apiUrl}Authenticate", content);
-            response.IsSuccessStatusCode.Equals(1);
-            var responseString = await response.Content.ReadAsStringAsync();
-            var result = responseString.ToJsonDeSerialize<OutResponse>();
-            result.ResponseCode.ShouldBeEquivalentTo(0);
-        }
         [Fact]
         public async Task Incorrect_UserId_ReturnCode_NotValid()
         {
@@ -79,44 +61,51 @@ namespace LoginService.API.Integration.Tests.Controllers
                 MethodId = 1,
                 IsEncrypt = false,
                 RequestData = "{\"user_id\":\"LmTzLdp6IF5kHqiShhkkAw==\",\"client_id\":\"FINOMER\",\"ECBBlockEncryption\":true}"
-                
+
             };
 
             var eventJson = @userRequest.ToJsonSerialize();
             HttpContent content = new StringContent(eventJson, Encoding.UTF8, "application/json");
             var response = await client.PostAsync($"{apiUrl}Authenticate", content);
-            //response.IsSuccessStatusCode.Equals(1);
-            var responseString = await response.Content.ReadAsStringAsync();
-            var result = responseString.ToJsonDeSerialize<OutResponse>();
-            result.ResponseData.ShouldNotBeEmpty();
-            response.RequestMessage.Equals("User not found");
+            response.EnsureSuccessStatusCode();
 
-            //result.ResponseCode.Equals(0);
-            //result.ResponseCode.ShouldBeEquivalentTo(0);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldNotBeNullOrEmpty();
+
+            var result = responseString.ToJsonDeSerialize<OutResponse>();
+            result.ResponseCode.ShouldBeEquivalentTo(1);
+            result.ResponseData.ShouldNotBeNullOrEmpty();
+            result.ResponseMessage.ShouldBeEquivalentTo("User not found");
         }
 
+        #endregion
+
+        #region Method 2
         [Fact]
-        
-        public async Task ResponseData_null_ReturnCode_NotValid()
+        public async Task Validate_User_Password_ReturnsSuccessResult()
         {
             var client = _factory.CreateDefaultClient();
 
             var @userRequest = new AuthenticationRequest()
             {
-                RequestId = "100032353_6292022153038769",
+                RequestId = "100032353_711202216413532",
                 TellerId = "100032353",
-                MethodId = 1,
+                MethodId = 2,
                 IsEncrypt = false,
-                RequestData = "{\"user_id\":\"Gipxpl7iWL62b/ldd5suEA==\",\"client_id\":\"FINOTLR\",\"ECBBlockEncryption\":true}"
+                RequestData = "{\"user_id\":\"K7CFFkt/XuWxCJZLlXpFQg==\",\"password\":\"fJx6WgNHnWnYV/aRNnaBmA==\",\"client_id\":\"FINOMER\",\"systemInfo\":{\"Channel\":\"2\",\"ISP\":\"Fino\",\"browser\":\"chrome 103.0.0.0\",\"Lattitude\":38.9847719,\"Longitude\":-77.5619419,\"version\":\"103.0.0.0\",\"PostalCode\":\"\",\"MAC_DeviceID\":\"12345\",\"CellID\":\"54321\",\"DeviceModel\":\"unknown\",\"DeviceOS\":\"windows\",\"MCC\":\"Test_MCC\",\"MNC\":\"Test_MNC\",\"LanguageSupported\":\"en-US\"},\"geoLocation\":{\"Lattitude\":38.9847719,\"Longitude\":-77.5619419},\"deviceId\":null,\"xauthToken\":null,\"biometric_fp\":{},\"otp\":{},\"Aadhaar\":{},\"ECBBlockEncryption\":true,\"EncType\":\"NEW\"}"
             };
 
             var eventJson = @userRequest.ToJsonSerialize();
             HttpContent content = new StringContent(eventJson, Encoding.UTF8, "application/json");
             var response = await client.PostAsync($"{apiUrl}Authenticate", content);
-            //response.IsSuccessStatusCode.Equals(1);
+            response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
+            responseString.ShouldNotBeNullOrEmpty();
             var result = responseString.ToJsonDeSerialize<OutResponse>();
-            result.ResponseData.IsEmpty();
+            result.ResponseData.ShouldNotBeNullOrEmpty();
+            result.ResponseCode.ShouldBeEquivalentTo(0);
         }
+
+        #endregion
     }
 }
